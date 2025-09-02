36d40aa2b1c65c09542c9d0ed93138298206205a
うつ見成矢 <utsumiseiya@utsumiseiyanoMacBook-Air.local>
2025-09-02 20:16:29 +0900

    disable eslint for build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   eslint: {
-    // 本番ビルド時にESLintエラーを無視する
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
